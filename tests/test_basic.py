e46fb19f416dbe1d24a5c7689d676ca82a53c9fe
Christian <bullcs95@gmail.com>
2019-06-22 11:48:44 -0400

    added 2018 game data

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,1 +1,10 @@
-print('whatthefuck')
+import pandas as pd
+
+head = list(range(1, 162))
+df = pd.read_csv(r"F:\Projects\Programming_projects\baseball_stats\game_data\data\game_data.csv")
+
+print(df.head())
+
+attend_avg = df['18'].mean()
+
+print(attend_avg)
