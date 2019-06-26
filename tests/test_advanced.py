66bc3cca3608de1abe4b2950ea3501d91cf39df9
Christian <bullcs95@gmail.com>
2019-06-25 21:00:01 -0400

    added score info

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -13,17 +13,34 @@ class Games:
         # basic info on the df
         num_of_games = len(df['Date'])
         num_of_teams = len(df.groupby('HomeTeam').nunique())
+        avg_duration = round(df['Duration'].mean(), 2)
+        avg_length_inn = round(df['LengthInOuts'].mean(), 2) / 6
 
-        # returns basic info
         stats = [
             num_of_games,
             num_of_teams,
+            avg_duration,
+            avg_length_inn,
         ]
 
         info = '\n'.join(map(str, stats))
         return info
 
 
-gm1 = Games(df)
+    def score_info(self):
+        avg_home_score = df['HomeRunsScore'].mean()
+        avg_away_scores = df['VisitorRunsScored'].mean()
 
-print(gm1.basic_info())
+        stats = [
+            avg_home_score,
+            avg_away_scores,
+        ]
+
+        info = '\n'.join(map(str, stats))
+        return info
+
+
+gm2018 = Games(df)
+
+print(gm2018.basic_info())
+print(gm2018.score_info())
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,3 +3,4 @@ from src.baseballdata import baseball_data_raw
 
 # 2018 mlb game data
 df = baseball_data_raw()
+
